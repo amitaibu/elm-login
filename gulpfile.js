0df2f3402dc252a8e86e77e014bde4483218b4be
Amitai Burstein <amitai@gizra.com>
2015-10-05 21:21:28 +0300

    uncomment wiredep

--- gulpfile.js
+++ gulpfile.js
@@ -214,7 +214,8 @@ gulp.task("serve:prod", function () {
 gulp.task("default", ["build", "serve:dev", "watch"]);
 
 // Builds the site but doesnt serve it to you
-gulp.task("build", ["clean:dev", "copy:dev", "elm", "styles", "bower"], function () {});
+// @todo: Add "bower" here
+gulp.task("build", ["clean:dev", "copy:dev", "elm", "styles"], function () {});
 
 // Builds your site with the "build" command and then runs all the optimizations on
 // it and outputs it to "./dist"
