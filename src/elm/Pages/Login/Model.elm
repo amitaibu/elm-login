8873ca44f9cec32820e803e653f4964a437422cf
David Hernández <davidhernandezruiz@gmail.com>
2016-11-09 18:32:18 +0100

    Adds the model for the Login

--- src/elm/Pages/Login/Model.elm
+++ src/elm/Pages/Login/Model.elm
@@ -1,12 +1,47 @@
 module Pages.Login.Model exposing (emptyModel, Model)
 
+import Http
+
+
+type alias AccessToken =
+    String
+
+
+type alias LoginForm =
+    { name : String
+    , pass : String
+    }
+
+
+type UserMessage
+    = None
+    | Error String
+
+
+type Status
+    = Init
+    | Fetching
+    | Fetched
+    | HttpError Http.Error
+
 
 type alias Model =
-    { login : String
+    { accessToken : AccessToken
+    , hasAccessTokenInStorage : Bool
+    , loginForm : LoginForm
+    , status : Status
+    , userMessage : UserMessage
     }
 
 
 emptyModel : Model
 emptyModel =
-    { login = ""
+    { accessToken =
+        ""
+        -- We start by assuming there's already an access token it the localStorage.
+        -- While this property is set to True, the login form will not appear.
+    , hasAccessTokenInStorage = True
+    , loginForm = LoginForm "demo" "1234"
+    , status = Init
+    , userMessage = None
     }
